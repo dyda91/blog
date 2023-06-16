485fb171dd5c9a472424195db745aa00e406ec9c
dyda91 <edjamasalmeida@outlook.com>
2023-06-15 21:15:35 -0300

    adda routes, getAll, get, delete and put

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -21,8 +21,86 @@ const PostController = {
         } catch (error) {
             console.log(error);
         }
-    }
+    },
 
+    getAll: async (req, res) => {
+
+        try {
+            const post = await Post.find();
+
+            res.json(post);
+
+        } catch (error) {
+            console.log(error)  ;          
+        }
+    },
+
+    get: async(req, res) => {
+
+        try {
+            
+            const id = req.params.id;
+            const post = await Post.findById(id);
+
+            if(!post) {
+                res.status(404).json({msg: "Postagem não encontrada"});
+                return;
+            }
+
+            res.json(post);
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    delete: async(req, res) => {
+
+        try {
+            
+            const id = req.params.id
+
+            const post = await Post.findById(id);
+
+            if(!post) {
+                res.status(404).json({msg: "Postagem não encontrada"});
+                return;
+            }
+
+
+            const deletePost = await Post.findByIdAndDelete(id);
+
+            res.status(200).json({deletePost, msg: "Postagem deletada com sucesso!"})
+
+        } catch (error) {
+            
+            console.log(error);
+
+        }
+    },
+
+    update:  async(req, res) => {
+
+        const id = req.params.id
+
+        const post = {
+            title: req.body.title,
+            subtitle: req.body.subtitle,
+            image: req.body.image,
+            text: req.body.text,
+            // user: req.body.user,
+        };
+
+        const updatePost = await Post.findByIdAndUpdate(id, post);
+
+        if(!updatePost) {
+            res.status(404).json({msg: "Postagem não encontrada"});
+            return;
+        }
+
+        res.status(200).json({Post, msg: "Postagem atualizada com sucesso!"})
+
+    }
     
 
 };
--- api/routes/post.js
+++ api/routes/post.js
@@ -7,4 +7,20 @@ router
     .route("/post")
     .post((req, res) => PostController.create(req, res));
 
+router
+   .route("/post")
+   .get((req, res) => PostController.getAll(req, res));
+
+router
+   .route("/post/:id")
+   .get((req, res) => PostController.get(req, res));
+
+router
+   .route("/post/:id")
+   .delete((req, res) => PostController.delete(req, res));
+
+router
+   .route("/post/:id")
+   .put((req, res) => PostController.update(req, res));
+
 module.exports = router
